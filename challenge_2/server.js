be9dc3a7d61e08c697ae36f23798689dc8830da4
Shiwei Chen <shiweichenus@gmail.com>
2022-05-18 21:13:48 -0500

    Connect Client to Server

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,23 +1,22 @@
-
-// Load HTTP module
-const http = require("http");
-
-// http://localhost:8000
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const cors = require('cors')
 const hostname = "127.0.0.1";
-const port = 8000;
+const port = 3000;
 
-// Create HTTP server
-const server = http.createServer(function(req, res) {
+app.use(express.static('client'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(cors())
 
-   // Set the response HTTP header with HTTP status and Content type
-   res.writeHead(200, {'Content-Type': 'text/plain'});
-
-   // Send the response body "Hello World"
-   res.end('Hello World\n');
+// Check if the server is working
+app.get('/', function(req, res) {
+   console.log(req.body);
+  res.send('Hello World!')
 });
 
-// Prints a log once the server starts listening
-server.listen(port, hostname, function() {
-   console.log(`Server running at http://${hostname}:${port}/`);
-})
-
+// Start server on a specified port
+app.listen(port, function() {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
