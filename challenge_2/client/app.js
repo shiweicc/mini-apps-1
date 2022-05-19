810c90f4690e053a74501f1fdf1a1ddcd90881c1
Shiwei Chen <shiweichenus@gmail.com>
2022-05-19 00:01:48 -0500

    POST request from client to server works

---
+++ challenge_2/client/app.js
@@ -0,0 +1,12 @@
+// $(document).ready(function () {
+
+//   document.getElementById('btn').onclick = function submit() {
+//     // document.getElementById('btn').textContent = "Help me";
+//     console.log(document.getElementById('input').value);
+
+//   }
+
+// });
+
+
+
--- challenge_2/server.js
+++ challenge_2/server.js
@@ -5,6 +5,7 @@ const cors = require('cors')
 const hostname = "127.0.0.1";
 const port = 3000;
 
+// app.set('view engine', 'ejs');
 app.use(express.static('client'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
@@ -12,9 +13,12 @@ app.use(cors())
 
 // Check if the server is working
 app.get('/', function(req, res) {
-   console.log(req.body);
   res.send('Hello World!')
 });
+
+app.post('/upload_json', function(req, res) {
+   res.send(req.body.input);
+})
 
 // Start server on a specified port
 app.listen(port, function() {
