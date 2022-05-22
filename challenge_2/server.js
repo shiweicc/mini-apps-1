0fc9315a46b8b532aaf51aa6d7e8c7991bf236d6
Shiwei Chen <shiweichenus@gmail.com>
2022-05-21 20:31:56 -0500

    Complete refactor form to use a file picker instead of a textarea

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const multer  = require('multer')
+const upload = multer({ dest: 'uploads/' }).single('uploaded_file');
 const app = express();
 const bodyParser = require('body-parser');
 const cors = require('cors')
@@ -20,23 +22,41 @@ app.get('/', function(req, res) {
   res.send('Hello World!')
 });
 
-
+// POST request to submit JSON file
 app.post('/', function(req, res) {
-
-  let data = JSON.parse(req.body.input);
-  let csv = flatten(data);
-
-  const filePath = path.join(__dirname, 'csvReports');
-
-  fs.writeFile(filePath + '/csvReoprt.csv', csv, (err) => {
+  upload(req, res, (err) => {
     if (err) {
-      console.log('Cannot write the csv report!');
+      console.log('Error uploading file.');
+      return;
     } else {
-      console.log('Writting the csv report succefully!');
+      console.log('File is uploaded!');
+
+      fs.readFile(req.file.path, "utf8", (err, data)=> {
+        if(err) {
+          console.log('Error reading the uploaded file!');
+          return;
+        } else {
+          console.log('Read file!')
+
+          var data = flatten(JSON.parse(data));
+          req.body = data;
+          var csv = req.body;
+
+          const filePath = path.join(__dirname, 'csvReports');
+
+          fs.writeFile(filePath + '/csvReoprt.csv', csv, (err) => {
+            if (err) {
+              console.log('Error writing csv report!');
+              return;
+            } else {
+              console.log('Write csv report!');
+              res.redirect('/');
+            }
+          })
+        }
+      })
     }
   })
-
-  res.redirect('/');
 })
 
 // Start server on a specified port
