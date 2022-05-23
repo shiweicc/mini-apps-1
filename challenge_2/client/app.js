fcdba52e6d94bdbb1e1fbed0c78979666fa47ba5
Shiwei Chen <shiweichenus@gmail.com>
2022-05-22 22:56:04 -0500

    Complete refactor to a single page application by using AJAX

--- challenge_2/client/app.js
+++ challenge_2/client/app.js
@@ -1,70 +1,36 @@
 
-// $(document).ready(function () {
+$(document).ready(function () {
 
-//   $('#form').submit((e) => {
-//     console.log('event: ', e);
+  $("#form").submit(function (e) {
 
-//     e.preventDefault();
+    e.preventDefault();
 
-//     // var form = $('#form')[0];
-//     // var formData = new FormData(form);
+    var formData = new FormData(this);
 
-//     // var files = $('#fileID')[0];
-//     // // var files = $('#fileID')[0];
-//     // console.log('files: ', files);
-//     // console.log('files[0]: ', files.files);
+    $.ajax({
+      type: "POST",
+      url: "/upload_json",
+      data: formData,
+      processData: false,
+      contentType: false,
+      enctype: 'multipart/form-data',
 
-//     // // Check file selected or not
-//     // if(files[0].length > 0 ){
-//     //   // form.append('file',files[0]);
-//     //   var form = new FormData(files);
-//     //   console.log('form here: ', form);
-//     // }
-//     // // console.log('my file: ', files[0]);
-//     // // var myFile = files[0];
+      success: function(data) {
+        console.log('Ajax process sucess!');
+     },
 
-//     // $.ajax({
-//     //   url: "/upload_json",
-//     //   type: "POST",
-//     //   data: form,
-//     //   processData: false,
-//     //   contentType: false,
+      error: function() {
+        console.log('Ajax process error!');
+      },
 
-//     //   success: function(data) {
-//     //     console.log(data);
-//     //     console.log('process sucess');
-//     //  },
+    });
 
-//     //   error: function() {
-//     //     console.log('process error');
-//     //   },
-
-//     // });
+  });
 
 
-//   });
-// });
+});
 
-// $.ajax({
-//   url: "/upload_json",
-//   type: "POST",
-//   dataType: "json",
-//   data: formData,
-//   contentType: "application/json",
-//   enctype: 'multipart/form-data',
-//   complete: function() {
-//     //called when complete
-//     console.log('process complete');
-//   },
 
-//   success: function(data) {
-//     console.log(data);
-//     console.log('process sucess');
-//  },
 
-//   error: function() {
-//     console.log('process error');
-//   },
-// });
 
 
--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const multer  = require('multer')
 const upload = multer({ dest: 'uploads/' }).single('uploaded_file');
+const fileUpload = require("express-fileupload");
 const app = express();
 const bodyParser = require('body-parser');
 const hostname = "127.0.0.1";
@@ -9,10 +10,10 @@ const flatten = require ('./flattenJSON');
 const fs = require('fs');
 const path = require('path');
 
-
 app.use(express.static('client'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(fileUpload());
 
 
 // Check if the server is working
@@ -20,44 +21,33 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 });
 
+
 // POST request to submit JSON file
-app.post('/upload_json', function(req, res) {
-  console.log('req.body: ', req);
-  upload(req, res, (err) => {
+/***Using express-fileupload***/
+app.post('/upload_json', (req, res) => {
+  console.log('req.files', req.files.uploaded_file);
+  console.log('here:', JSON.parse(req.files.uploaded_file.data));
+
+  if (!req.files) {
+    return res.status(400).send("No files were uploaded.");
+  }
+
+  var filePath = path.join(__dirname, 'csvReports');
+  var csv = flatten(JSON.parse(req.files.uploaded_file.data));
+
+
+  fs.writeFile(filePath + '/csvReoprt.csv', csv, (err) => {
     if (err) {
-      console.log('Error uploading file.');
+      console.log('Error writing csv report!');
       return;
     } else {
-      console.log('File is uploaded!');
-      console.log('here: ', req.body);
-
-      fs.readFile(req.file.path, "utf8", (err, data)=> {
-        if(err) {
-          console.log('Error reading the uploaded file!');
-          return;
-        } else {
-          console.log('Read file!')
-
-          var data = flatten(JSON.parse(data));
-          req.body = data;
-          var csv = req.body;
-
-          const filePath = path.join(__dirname, 'csvReports');
-
-          fs.writeFile(filePath + '/csvReoprt.csv', csv, (err) => {
-            if (err) {
-              console.log('Error writing csv report!');
-              return;
-            } else {
-              console.log('Write csv report!');
-              res.send(csv);
-            }
-          })
-        }
-      })
+      console.log('Write csv report!');
+      res.send(csv);
     }
   })
-})
+
+});
+
 
 // Start server on a specified port
 app.listen(port, function() {
@@ -70,7 +60,49 @@ app.listen(port, function() {
 
 
 
+/******* Using multer (Ajax doesn't work) *********/
+// app.post('/upload_json', function(req, res) {
+//   console.log('req.body: ', req.body);
+//   console.log('req.files', req.files.uploaded_file);
+//   console.log('here:', JSON.parse(req.files.uploaded_file.data));
 
+//   const filePath = path.join(__dirname, 'csvReports');
+
+//   upload(req, res, (err) => {
+//     if (err) {
+//       console.log('Error uploading file.');
+//       return;
+//     } else {
+//       console.log('File is uploaded!');
+//       console.log('here: ', req.body);
+
+//       fs.readFile(req.file.path, "utf8", (err, data)=> {
+//         if(err) {
+//           console.log('Error reading the uploaded file!');
+//           return;
+//         } else {
+//           console.log('Read file!')
+
+//           var data = flatten(JSON.parse(data));
+//           req.body = data;
+//           var csv = req.body;
+
+//           const filePath = path.join(__dirname, 'csvReports');
+
+//           fs.writeFile(filePath + '/csvReoprt.csv', csv, (err) => {
+//             if (err) {
+//               console.log('Error writing csv report!');
+//               return;
+//             } else {
+//               console.log('Write csv report!');
+//               res.send(csv);
+//             }
+//           })
+//         }
+//       })
+//     }
+//   })
+// })
 
 
 
