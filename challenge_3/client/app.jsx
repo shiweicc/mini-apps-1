3ca2f3e6cb2989778c10ab17bdcbf8275f34eebd
Shiwei Chen <shiweichenus@gmail.com>
2022-06-14 22:07:48 -0500

    Update lables

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -6,12 +6,21 @@ class App extends React.Component {
     super(props);
     this.state = {
       checkout_clicked: false,
-      form1_filled: false,
-      form2_filled: false,
-      form3_filled: false,
       form1_next: false,
       form2_next: false,
       confirmBtn: false,
+      fullname:'',
+      password:'',
+      address_line1:'',
+      address_line2:'',
+      city:'',
+      state:'',
+      zipcode:'',
+      phone_number:'',
+      credit_card_num :'',
+      expiry_date:'',
+      CVV:'',
+      billing_zipcode:'',
     }
   }
   handleCheckoutBtn(e) {
@@ -58,11 +67,11 @@ const Form1 = (props) => {
     <div>
       <h3>Please fill out Form #1:</h3>
       <form>
-        <label htmlFor="name">Enter your fullname: </label>
+        <label htmlFor="name">Full name: </label>
           <input type="text" name="name" id="name" required />
-          <label htmlFor="email">Enter your email: </label>
+          <label htmlFor="email">Email: </label>
           <input type="text" name="email" id="email" required />
-          <label htmlFor="password">Enter your password: </label>
+          <label htmlFor="password">Password: </label>
           <input type="text" name="password" id="password" required />
 
           <input type="submit" value="Next" onClick={(e)=>{props.handleNextForm1(e)}}/>
@@ -84,15 +93,17 @@ const Form2 = (props) => {
     <div>
       <h3>Please fill out Form #2:</h3>
       <form>
-        <label htmlFor="address_line1">Enter your address: </label>
+        <label htmlFor="address_line1">Address: </label>
           <input type="text" name="address_line1" id="address_line1" required />
-          <label htmlFor="address_line2">Enter your address (optional): </label>
+          <label htmlFor="address_line2">Address (optional): </label>
           <input type="text" name="address_line2" id="address_line2" />
-          <label htmlFor="city">Enter your city: </label>
+          <label htmlFor="city">City: </label>
           <input type="text" name="city" id="city" required />
-          <label htmlFor="zipcode">Enter your zipcode: </label>
+          <label htmlFor="state">State: </label>
+          <input type="text" name="state" id="state" required />
+          <label htmlFor="zipcode">Zipcode: </label>
           <input type="text" name="zipcode" id="zipcode" required />
-          <label htmlFor="phone_number">Enter your phone number: </label>
+          <label htmlFor="phone_number">Phone number: </label>
           <input type="text" name="phone_number" id="phone_number" required />
 
           <input type="submit" value="Next" onClick={(e)=>{props.handleNextForm2(e)}}/>
@@ -112,11 +123,13 @@ const Form3 = (props) => {
     <div>
       <h3>Please fill out Form #3:</h3>
       <form>
-        <label htmlFor="credit_card_num">Enter your credit card number: </label>
+          <label htmlFor="credit_card_num">Credit card number: </label>
           <input type="text" name="credit_card_num" id="credit_card_num" required />
-          <label htmlFor="CVV">Enter your CVV: </label>
+          <label htmlFor="expiry_date ">Expiry date: </label>
+          <input type="text" name="expiry_date " id="expiry_date " />
+          <label htmlFor="CVV">CVV: </label>
           <input type="text" name="CVV" id="CVV" />
-          <label htmlFor="billing_zipcode">Enter your billing zipcode: </label>
+          <label htmlFor="billing_zipcode">Billing zipcode: </label>
           <input type="text" name="billing_zipcode" id="billing_zipcode" required />
 
           <input type="submit" value="Confirm" onClick={(e)=>{props.handleConfirm(e)}}/>
